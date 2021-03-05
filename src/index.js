77ce1c8b17a4dea8e9902f32dd5d780c2e85a99a
Roman-Y-K <r.prohor7@gmail.com>
2021-03-05 14:50:03 +0200

    проверка линтинга 2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    {/* <App /> */}
-  </React.StrictMode>,
-  document.getElementById('root')
+  <React.StrictMode>{/* <App /> */}</React.StrictMode>,
+  document.getElementById('root'),
 );
-
